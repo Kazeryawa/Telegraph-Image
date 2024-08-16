abb73c4f793e071ccb6cda929b99e4955c6fb750
Kazery <kazery@qq.com>
2024-08-16 18:18:40 +0800

    Update index.js

--- functions/api/bing/wallpaper/index.js
+++ functions/api/bing/wallpaper/index.js
@@ -8,7 +8,7 @@ export async function onRequest(context) {
       next, // used for middleware or to fetch assets
       data, // arbitrary space for passing data between middlewares
     } = context;
-    const res = await fetch(`https://t.alcy.cc/ycy/`);
+    const res = await fetch(`https://t.alcy.cc/ycy?json`);
     const bing_data = await res.json();
     const return_data={
         "status":true,
